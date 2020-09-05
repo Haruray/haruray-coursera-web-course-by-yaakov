17114451df187adc679d712f298ccb9860a6fbb8
Safiq F <syafiqfaray2@gmail.com>
2020-09-05 20:50:56 +0700

    poggers

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -102,7 +102,6 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories)['short_name'];
-      console.log(chosenCategoryShortName);
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -117,7 +116,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       var randomCategoryShortName = "randomCategoryShortName";
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName",chosenCategoryShortName);
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName",String(chosenCategoryShortName));
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
